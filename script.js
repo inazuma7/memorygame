f39698d0a11e4f5ddba1c4749b1bc87108c5ae4f
nur aleina <nuraleina77@gmail.com>
2023-07-01 14:09:05 +0800

    update

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ function shuffleCard() {
     flipsTag.innerText = flips;
     disableDeck = isPlaying = false;
 
-    let arr = [1, 2, 3, 4, 5, 6,7,8, 1, 2, 3, 4, 5, 6,7,8];
+    let arr = [1, 2, 3, 4, 5, 6,7,8, 1, 2, 3, 4, 5, 6, 7, 8];
     arr.sort(() => Math.random() > 0.5 ? 1 : -1);
 
     cards.forEach((card, index) => {
